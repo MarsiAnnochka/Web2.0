b33b494143a30edaf579aca172a722a5e4d735a7
MarsiAnnochka <egorova.aanna@yandex.ru>
2021-02-24 14:28:56 -0500

    first lecture

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,9 @@
+import {IsNotEmpty} from "class-validator";
+import {ApiProperty} from "@nestjs/swagger";
+
+export class CreateUserDto {
+  @ApiProperty() @IsNotEmpty()
+  name: string;
+  @ApiProperty() @IsNotEmpty()
+  password: string;
+}
