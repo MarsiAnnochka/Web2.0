08ec411efa60af5bc7fdc916bb8d5c11633f67da
MarsiAnnochka <egorova.aanna@yandex.ru>
2021-04-06 05:28:45 -0400

    merge

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { AppGateway } from './app.gateway';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), AuthModule, UsersModule],
   controllers: [],
-  providers: [],
+  providers: [AppGateway],
 })
 export class AppModule {}
