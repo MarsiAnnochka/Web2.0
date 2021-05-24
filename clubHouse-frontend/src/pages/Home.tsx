8b20ed409debdd264ba4c4cb1ecc8ecd4160a0e3
MarsiAnnochka <egorova.aanna@yandex.ru>
2021-05-24 16:29:46 -0400

    images

--- clubHouse-frontend/src/pages/Home.tsx
+++ clubHouse-frontend/src/pages/Home.tsx
@@ -10,21 +10,21 @@ export const Home: React.FC = () => (
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="/1.png"
+                        src="/public/1.png"
                         alt="First slide"
                     />
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="/2.jpg"
+                        src="/public/2.jpg"
                         alt="Second slide"
                     />
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="/3.jpg"
+                        src="/public/3.jpg"
                         alt="Third slide"
                     />
                 </Carousel.Item>
