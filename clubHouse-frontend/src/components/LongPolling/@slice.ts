c0ae20b92d5c798281d26d62422319f8552f6f81
Anastasia <anasteisha.i.g@mail.ru>
2021-06-08 23:52:46 +0300

    depl.. ya plachu

--- clubHouse-frontend/src/components/LongPolling/@slice.ts
+++ clubHouse-frontend/src/components/LongPolling/@slice.ts
@@ -47,6 +47,8 @@ export const getMessage = createAsyncThunk(
             console.log("FINISHED");
             console.log(messages)
         } catch (err) {
+            console.log(messages)
+            
             console.log('Error' + err)
         }
     }
